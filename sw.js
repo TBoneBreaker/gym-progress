58ec3f4acdf67d335200b64ca9881bfc89ce4b71
TBoneBreaker <thomas.steinert.05@outlook.de>
2026-09-01 19:10:01 +0200

    Load responsive 7-day bodyweight chart

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="gym-progress-v10";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png","./patch-v10.js"];
+const CACHE="gym-progress-v11";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png","./patch-v10.js","./patch-v11.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 async function documentResponse(request){
@@ -16,6 +16,7 @@ async function documentResponse(request){
   if(!type.includes("text/html"))return response;
   let html=await response.text();
   if(!html.includes("patch-v10.js"))html=html.replace("</body>",'<script src="./patch-v10.js"></script></body>');
+  if(!html.includes("patch-v11.js"))html=html.replace("</body>",'<script src="./patch-v11.js"></script></body>');
   const headers=new Headers(response.headers);headers.delete("content-length");headers.set("content-type","text/html; charset=utf-8");
   return new Response(html,{status:response.status,statusText:response.statusText,headers});
 }
