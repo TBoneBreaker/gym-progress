4b7a684016f352ef6ceaa040796c628ccb4627b5
TBoneBreaker <thomas.steinert.05@outlook.de>
2026-09-01 20:08:05 +0200

    Refresh PWA nutrition persistence

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="gym-progress-v14";
-const ASSETS=["./","./index.html","./app-base.html","./manifest.webmanifest","./icon-180.png","./icon-512.png","./patch-v14.js"];
+const CACHE="gym-progress-v15";
+const ASSETS=["./","./index.html","./app-base.html","./manifest.webmanifest","./icon-180.png","./icon-512.png","./patch-v14.js","./patch-v15.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
